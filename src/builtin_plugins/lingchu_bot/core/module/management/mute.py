f4976ce4f59dd35fd7127243afd2adf5ee62468d
xinvxueyuan <xinvxueyuan@yeah.net>
2026-01-16 16:39:23 +0800

    feat: 完成发言模块基本功能

--- src/builtin_plugins/lingchu_bot/core/module/management/mute.py
+++ src/builtin_plugins/lingchu_bot/core/module/management/mute.py
@@ -1,5 +1,5 @@
 from nonebot import logger, on_startswith
-from nonebot.adapters.onebot.v11 import GroupMessageEvent
+from nonebot.adapters.onebot.v11 import Bot, GroupMessageEvent
 from nonebot_plugin_alconna.uniseg import UniMessage
 
 ban_cmd = on_startswith("禁言", priority=5, block=True)
@@ -8,33 +8,63 @@ unban_cmd = on_startswith("解禁", priority=5, block=True)
 whole_unban_cmd = on_startswith("全体解禁", priority=5, block=True)
 
 
-# TODO: 实现具体功能
+# TODO: 增强：请求开关机状态、权限验证等
 @ban_cmd.handle()
-async def handle_mute(event: GroupMessageEvent) -> None:
+async def handle_mute(bot: Bot, event: GroupMessageEvent) -> None:
+    import re
+
+    pattern = r"禁言\[CQ:at,qq=(\d+),name=[^\]]+\] (\d+)"
+    match = re.search(pattern, event.raw_message)
+    if not match:
+        await UniMessage.text("格式错误，请使用：禁言@某人 时间（单位：秒）").send(
+            at_sender=True, reply_to=True
+        )
+        return
+    target_user = int(match.group(1))
+    mute_time = int(match.group(2))
     logger.debug(
         f"{event.self_id}收到禁言指令: {event.raw_message}\
-来自用户: {event.user_id} 在群: {event.group_id}"
+来自用户: {event.user_id}目标用户: {target_user}时间：{mute_time}在群: {event.group_id}"
     )
-    # 禁言\[CQ:at,qq=(\d+),name=([^\]]+)\] (\d+)
-    await UniMessage.text("禁言成功").send(at_sender=True, reply_to=True)
+    if await bot.set_group_ban(
+        group_id=event.group_id, user_id=target_user, duration=mute_time
+    ):
+        await UniMessage.text("禁言成功").send(at_sender=True, reply_to=True)
 
 
 @whole_ban_cmd.handle()
-async def handle_whole_mute() -> None:
-    message = UniMessage.text("全体禁言成功")
-    await message.send(at_sender=True, reply_to=True)
+async def handle_whole_mute(bot: Bot, event: GroupMessageEvent) -> None:
+    if await bot.set_group_whole_ban(group_id=event.group_id, enable=True):
+        await UniMessage.text("全体禁言成功").send(at_sender=True, reply_to=True)
 
 
 @unban_cmd.handle()
-async def handle_unmute() -> None:
-    message = UniMessage.text("解禁成功")
-    await message.send(at_sender=True, reply_to=True)
+async def handle_unmute(bot: Bot, event: GroupMessageEvent) -> None:
+    import re
+
+    # 匹配格式: 禁言[CQ:at,qq=123456,name=xxx] 60
+    pattern = r"解禁\[CQ:at,qq=(\d+),name=[^\]]+\] "
+    match = re.search(pattern, event.raw_message)
+    if not match:
+        await UniMessage.text("格式错误，请使用：解禁@某人").send(
+            at_sender=True, reply_to=True
+        )
+        return
+    target_user = int(match.group(1))
+    logger.debug(
+        f"{event.self_id}收到解禁指令: {event.raw_message}\
+来自用户: {event.user_id}目标用户: {target_user}在群: {event.group_id}"
+    )
+    if await bot.set_group_ban(
+        group_id=event.group_id, user_id=target_user, duration=0
+    ):
+        await UniMessage.text("解禁成功").send(at_sender=True, reply_to=True)
 
 
 @whole_unban_cmd.handle()
-async def handle_whole_unmute() -> None:
-    message = UniMessage.text("全体解禁成功")
-    await message.send(at_sender=True, reply_to=True)
+async def handle_whole_unmute(bot: Bot, event: GroupMessageEvent) -> None:
+    if await bot.set_group_whole_ban(group_id=event.group_id, enable=False):
+        await UniMessage.text("全体解禁成功").send(at_sender=True, reply_to=True)
 
 
 # from nonebot_plugin_alconna import on_alconna
