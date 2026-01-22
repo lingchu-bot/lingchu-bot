bcba50cda1cc366692188ec4b1b28cbbb04361ec
xinvxueyuan <xinvxueyuan@yeah.net>
2026-01-22 21:32:17 +0800

    feat: 添加公告功能，更新管理模块，优化导入和导出逻辑

--- src/builtin_plugins/lingchu_bot/core/module/management/__init__.py
+++ src/builtin_plugins/lingchu_bot/core/module/management/__init__.py
@@ -1,3 +1,3 @@
-from . import admin, ban, kick, special_title
+from . import admin, ban, kick, notice, special_title
 
-__all__ = ["admin", "ban", "kick", "special_title"]
+__all__ = ["admin", "ban", "kick", "notice", "special_title"]
--- src/builtin_plugins/lingchu_bot/core/module/management/utils/msg_seg.py
+++
@@ -1,3 +0,0 @@
-from nonebot.adapters.onebot.v11 import GroupMessageEvent, MessageSegment
-
-MessageSegment.at(GroupMessageEvent.user_id)
